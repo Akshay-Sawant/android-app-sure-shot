506f4e27428c43cf52ad51506f7468389121642c
Suraj Suryawanshi <surajsuryawanshi16@gmail.com>
2020-05-21 19:27:38 +0530

    Recharge second screen added

--- app/src/main/java/com/sureshots/app/fragments/RechargeTwoFragment.kt
+++ app/src/main/java/com/sureshots/app/fragments/RechargeTwoFragment.kt
@@ -5,18 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.sureshots.app.R
+import kotlinx.android.synthetic.main.fragment_recharge_two.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class RechargeTwoFragment : Fragment(R.layout.fragment_recharge_two) {
+class RechargeTwoFragment : Fragment(R.layout.fragment_recharge_two),View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        view.buttonProceed.setOnClickListener(this)
 
 
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.buttonProceed -> view?.let {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_rechargeTwoFragment_to_subscriptionFragment)
+            }
+        }
     }
 
 }
