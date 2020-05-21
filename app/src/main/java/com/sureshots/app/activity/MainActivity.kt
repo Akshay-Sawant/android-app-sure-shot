d9c007826c2694b6aff43347e285dbde7a8811bc
Suraj Suryawanshi <surajsuryawanshi16@gmail.com>
2020-05-21 13:45:19 +0530

    Added navigation drawer in main activity

--- app/src/main/java/com/sureshots/app/activity/MainActivity.kt
+++ app/src/main/java/com/sureshots/app/activity/MainActivity.kt
@@ -1,29 +1,59 @@
 package com.sureshots.app.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import com.sureshots.app.R
 
-class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener,
+    NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var mNavController: NavController
+
+    private lateinit var toolbar: Toolbar
+
+    private lateinit var drawerLayout: DrawerLayout
+
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
 
-        mNavController = this@MainActivity.findNavController(R.id.sure_shot_nav_host)
-        NavigationUI.setupActionBarWithNavController(this@MainActivity, mNavController)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigationView)
+
+        mNavController = Navigation.findNavController(this,R.id.sure_shot_nav_host)
+        NavigationUI.setupActionBarWithNavController(this, mNavController, drawerLayout)
+        NavigationUI.setupWithNavController(navigationView, mNavController)
+        navigationView.setNavigationItemSelectedListener(this)
         mNavController.addOnDestinationChangedListener(this@MainActivity)
 
     }
 
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     override fun onSupportNavigateUp(): Boolean {
-        return mNavController.navigateUp()
+        return NavigationUI.navigateUp(mNavController, drawerLayout)
     }
 
     override fun onDestinationChanged(
@@ -35,5 +65,17 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
             R.id.SignUpFragment -> supportActionBar?.hide()
             else -> supportActionBar?.show()
         }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        menuItem.isChecked = true
+        drawerLayout.closeDrawers()
+        val id = menuItem.itemId
+        when (id) {
+            R.id.first -> mNavController.navigate(R.id.RechargeOneFragment)
+            R.id.second -> mNavController.navigate(R.id.RechargeTwoFragment)
+            R.id.third -> mNavController.navigate(R.id.ReferEarnFragment)
+        }
+        return true
     }
 }
