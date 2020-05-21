86350a2696bbc0f01a03b09898c57d9965935a68
Suraj Suryawanshi <surajsuryawanshi16@gmail.com>
2020-05-21 13:46:32 +0530

    Added recharge screen UI second

---
+++ app/src/main/java/com/sureshots/app/fragments/RechargeTwoFragment.kt
@@ -0,0 +1,22 @@
+package com.sureshots.app.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.sureshots.app.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RechargeTwoFragment : Fragment(R.layout.fragment_recharge_two) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
