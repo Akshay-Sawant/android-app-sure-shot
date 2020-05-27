f8feb7319f5d527ebfa63a8450dabc24a51f5279
Suraj Suryawanshi <surajsuryawanshi16@gmail.com>
2020-05-27 18:42:27 +0530

    Added form validation for signup

--- app/src/main/java/com/sureshots/app/activity/SignUpActivity.kt
+++ app/src/main/java/com/sureshots/app/activity/SignUpActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.widget.Toast
 import com.sureshots.app.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -27,6 +29,22 @@ class SignUpActivity : AppCompatActivity(),View.OnClickListener {
     override fun onClick(view: View) {
         when(view.id){
             R.id.buttonContinue -> startActivity(VerifyOTPActivity.newIntent(this))
+        }
+    }
+
+    private fun signUp(){
+        textInputLayoutMobileNumber.error =null
+        val number = textInputMobileNumber.text.toString().trim()
+        var hasError = false
+        if(number == ""){
+            textInputLayoutMobileNumber.error = "Enter your mobile number!"
+            hasError = true
+        }
+        if (hasError) {
+            val toast = Toast.makeText(this, "Please correct highlighted fields!", Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.show()
+            return
         }
     }
 }
