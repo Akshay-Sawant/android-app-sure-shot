5bb740a872781e13804b90fe8e92223cc7c04e26
Suraj Suryawanshi <surajsuryawanshi16@gmail.com>
2020-06-17 18:54:48 +0530

    Code changed to display info after transaction completed showing transaction on success screen

--- app/src/main/java/com/sureshots/app/activity/PaymentSuccessActivity.kt
+++ app/src/main/java/com/sureshots/app/activity/PaymentSuccessActivity.kt
@@ -11,10 +11,26 @@ import kotlinx.android.synthetic.main.activity_payment_success.*
 
 class PaymentSuccessActivity : AppCompatActivity(),View.OnClickListener {
 
+    private lateinit var pay_txnid: String
+    private lateinit var pay_firstname: String
+    private lateinit var pay_status: String
+    private lateinit var pay_phone: String
+    private lateinit var pay_amount: String
+
     companion object {
         private lateinit var mIntent: Intent
-        fun newIntent(context: Context): Intent {
+        fun newIntent(context: Context,
+                      pay_txnid: String,
+                      pay_firstname: String,
+                      pay_status: String,
+                      pay_phone: String,
+                      pay_amount: String): Intent {
             mIntent = Intent(context, PaymentSuccessActivity::class.java)
+            mIntent.putExtra("pay_txnid",pay_txnid)
+            mIntent.putExtra("pay_firstname",pay_firstname)
+            mIntent.putExtra("pay_status",pay_status)
+            mIntent.putExtra("pay_phone",pay_phone)
+            mIntent.putExtra("pay_amount",pay_amount)
             return mIntent
         }
     }
@@ -24,6 +40,13 @@ class PaymentSuccessActivity : AppCompatActivity(),View.OnClickListener {
         setContentView(R.layout.activity_payment_success)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         buttonContinue.setOnClickListener(this)
+        pay_txnid = intent.getStringExtra("pay_txnid") as String
+        pay_firstname = intent.getStringExtra("pay_firstname") as String
+        pay_status = intent.getStringExtra("pay_status") as String
+        pay_phone = intent.getStringExtra("pay_phone") as String
+        pay_amount = intent.getStringExtra("pay_amount") as String
+        textViewMobileNumber.text = pay_phone
+        textViewOrderId.text = getString(R.string.text_order_id,pay_txnid)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
