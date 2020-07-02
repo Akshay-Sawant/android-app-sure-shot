b8323a9b94b1e592faf3c75a4d6c5949ea84c72e
Akshay Sawant <akshaysawant003@gmail.com>
2020-07-02 20:21:48 +0530

    SignInFragment.kt: - added sign up path at the bottom - bind the path with respect to click listener

--- app/src/main/java/com/sureshots/app/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/sureshots/app/ui/signin/SignInFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.sureshots.app.R
@@ -15,6 +16,8 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in), View.OnClickListener
 
     private lateinit var mButtonSignInContinue: Button
 
+    private lateinit var mTextViewSignInNoAccount: TextView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -25,11 +28,17 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in), View.OnClickListener
 
         mButtonSignInContinue = view.findViewById(R.id.buttonSignInContinue)
         mButtonSignInContinue.setOnClickListener(this@SignInFragment)
+
+        mTextViewSignInNoAccount = view.findViewById(R.id.textViewSignInNoAccount)
+        mTextViewSignInNoAccount.setOnClickListener(this@SignInFragment)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.buttonSignInContinue -> {
+
+            }
+            R.id.textViewSignInNoAccount -> {
 
             }
         }
