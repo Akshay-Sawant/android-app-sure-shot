d12170223a88519183d6e0a583dbd19ff21d2af6
Akshay Sawant <akshaysawant003@gmail.com>
2020-07-02 16:46:14 +0530

    SignUpFragment.kt: - updated the UI - resolved all the issues - updated teh strings.xml file

--- app/src/main/java/com/sureshots/app/fragments/auth/SignUpFragment.kt
+++ app/src/main/java/com/sureshots/app/fragments/auth/SignUpFragment.kt
@@ -3,19 +3,29 @@ package com.sureshots.app.fragments.auth
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Button
 import androidx.navigation.Navigation
-
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.sureshots.app.R
-import kotlinx.android.synthetic.main.fragment_sign_up.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class SignUpFragment : Fragment(R.layout.fragment_sign_up),View.OnClickListener {
+class SignUpFragment : Fragment(R.layout.fragment_sign_up), View.OnClickListener {
+
+    private lateinit var mTextInputLayoutMobileNumber: TextInputLayout
+    private lateinit var mTextInputEditTextMobileNumber: TextInputEditText
+
+    private lateinit var mButtonContinue: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.buttonContinue.setOnClickListener(this)
+        mTextInputLayoutMobileNumber = view.findViewById(R.id.textInputLayoutMobileNumber)
+        mTextInputEditTextMobileNumber = view.findViewById(R.id.textInputEditTextMobileNumber)
+
+        mButtonContinue = view.findViewById(R.id.buttonContinue)
+        mButtonContinue.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -26,5 +36,4 @@ class SignUpFragment : Fragment(R.layout.fragment_sign_up),View.OnClickListener 
             }
         }
     }
-
 }
