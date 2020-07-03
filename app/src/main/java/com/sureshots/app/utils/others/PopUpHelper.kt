f00d69f67fe72a3eda8bd10b15205005250c9f16
Akshay Sawant <akshaysawant003@gmail.com>
2020-07-03 17:07:19 +0530

    Created PopUpHelper.kt class file for handling all toasts and snackbars

---
+++ app/src/main/java/com/sureshots/app/utils/others/PopUpHelper.kt
@@ -0,0 +1,38 @@
+package com.sureshots.app.utils.others
+
+import android.content.Context
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class PopUpHelper(private val mContext: Context) {
+
+    /**
+     * Toast
+     * @param mMessage
+     * */
+    fun onShortToast(mMessage: String) {
+        return Toast.makeText(mContext, mMessage, Toast.LENGTH_SHORT).show()
+    }
+
+    fun onLongToast(mMessage: String) {
+        return Toast.makeText(mContext, mMessage, Toast.LENGTH_LONG).show()
+    }
+
+    /**
+     * Snackbar
+     * @param mView
+     * @param mMessage
+     * */
+    fun onShortSnackbar(mView: ViewGroup, mMessage: String) {
+        Snackbar.make(mView, mMessage, Snackbar.LENGTH_SHORT).show()
+    }
+
+    fun onLongSnackbar(mView: ViewGroup, mMessage: String) {
+        Snackbar.make(mView, mMessage, Snackbar.LENGTH_LONG).show()
+    }
+
+    fun onIndefiniteSnackbar(mView: ViewGroup, mMessage: String, mDuration: Int) {
+        Snackbar.make(mView, mMessage, Snackbar.LENGTH_INDEFINITE).show()
+    }
+}
