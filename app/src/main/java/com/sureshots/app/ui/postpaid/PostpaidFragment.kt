bf8161ad71ddde58a7dfe1dca3cf090bb8daff33
Akshay Sawant <akshaysawant003@gmail.com>
2020-07-20 16:52:42 +0530

    PostpaidFragment.kt: - Updated the Ui by removing all the static content - removed unwanted code - added click listeners on text and button - navigation to next screen as well as popped to back stack to change the preference - added validations but disabled it

--- app/src/main/java/com/sureshots/app/ui/postpaid/PostpaidFragment.kt
+++ app/src/main/java/com/sureshots/app/ui/postpaid/PostpaidFragment.kt
@@ -1,55 +1,76 @@
 package com.sureshots.app.ui.postpaid
 
 import android.os.Bundle
-import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.Navigation
-
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.sureshots.app.R
-//import com.sureshots.app.ui.activity.RechargeTwoActivity
-import kotlinx.android.synthetic.main.fragment_postpaid.*
-import kotlinx.android.synthetic.main.fragment_postpaid.view.*
+import com.sureshots.app.utils.others.ValidationUtils
+import de.hdodenhof.circleimageview.CircleImageView
 
 /**
  * A simple [Fragment] subclass.
  */
-class PostpaidFragment : Fragment(R.layout.fragment_postpaid),View.OnClickListener {
+class PostpaidFragment : Fragment(R.layout.fragment_postpaid), View.OnClickListener {
+
+    private lateinit var mCircleImageViewPostPaid: CircleImageView
+    private lateinit var mTextViewPostPaidCompanyName: TextView
+    private lateinit var mTextViewPostPaidChange: TextView
+
+    private lateinit var mTextInputLayoutPostPaidMobileNumber: TextInputLayout
+    private lateinit var mTextInputEditTextPostPaidMobileNumber: TextInputEditText
+
+    private lateinit var mButtonPostPaidProceed: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.buttonProceed.setOnClickListener(this)
-        view.textViewChange.setOnClickListener(this)
+
+        mCircleImageViewPostPaid = view.findViewById(R.id.circleImageViewPostPaidCompanyLogo)
+        mTextViewPostPaidCompanyName = view.findViewById(R.id.textViewPostPaidCompanyName)
+        mTextViewPostPaidChange = view.findViewById(R.id.textViewPostPaidChange)
+        mTextViewPostPaidChange.setOnClickListener(this@PostpaidFragment)
+
+        mTextInputLayoutPostPaidMobileNumber =
+            view.findViewById(R.id.textInputLayoutPostPaidMobileNumber)
+        mTextInputEditTextPostPaidMobileNumber =
+            view.findViewById(R.id.textInputEditTextPostPaidMobileNumber)
+
+        mButtonPostPaidProceed = view.findViewById(R.id.buttonPostPaidProceed)
+        mButtonPostPaidProceed.setOnClickListener(this@PostpaidFragment)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.buttonProceed -> view?.let {
-                postPaidNumber()
-                //Navigation.findNavController(it).navigate(R.id.action_rechargeOneFragment_to_rechargeTwoFragment)
+            R.id.textViewPostPaidChange -> view?.let {
+                Navigation.findNavController(it)
+                    .popBackStack(R.id.action_rechargeOneFragment_to_dashboardFragment, false)
             }
-            R.id.textViewChange -> view?.let {
-                //startActivity(DashboardMainActivity.newIntentFromPostpaid(requireContext()))
-                Navigation.findNavController(it).navigate(R.id.action_rechargeOneFragment_to_dashboardFragment)
+            R.id.buttonPostPaidProceed -> view?.let {
+//                isPostpaidValidated()
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_rechargeOneFragment_to_rechargeTwoFragment)
             }
         }
     }
 
-    private fun postPaidNumber(){
-        textInputLayoutMobileNumber.error =null
-        val number = textInputMobileNumber.text.toString().trim()
-        var hasError = false
-        if(number == ""){
-            textInputLayoutMobileNumber.error = "Enter your mobile number!"
-            hasError = true
+    private fun isPostpaidValidated() {
+        when {
+            !ValidationUtils.getValidationUtils()
+                .isInputEditTextMobileFunc(
+                    mTextInputLayoutPostPaidMobileNumber,
+                    mTextInputEditTextPostPaidMobileNumber,
+                    getString(R.string.text_error_mobile)
+                ) -> return
+            else -> {
+                view?.let {
+                    Navigation.findNavController(it)
+                        .navigate(R.id.action_rechargeOneFragment_to_rechargeTwoFragment)
+                }
+            }
         }
-        if (hasError) {
-            val toast = Toast.makeText(requireContext(), "Please correct highlighted fields!", Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.CENTER, 0, 0)
-            toast.show()
-            return
-        }
-//        startActivity(RechargeTwoActivity.newIntentNewTask(requireContext(),number))
     }
 }
