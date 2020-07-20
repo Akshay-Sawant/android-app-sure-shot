aa004ffdd2a0c07fc275584c8a59092725f24f24
Akshay Sawant <akshaysawant003@gmail.com>
2020-07-20 19:00:35 +0530

    SubscriptionFragment.kt: - updated the ui by removing all the static content - removed unwanted code - binded the UI

--- app/src/main/java/com/sureshots/app/ui/subscription/SubscriptionFragment.kt
+++ app/src/main/java/com/sureshots/app/ui/subscription/SubscriptionFragment.kt
@@ -3,25 +3,27 @@ package com.sureshots.app.ui.subscription
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Button
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 
 import com.sureshots.app.R
 import com.sureshots.app.data.model.Subscription
-import kotlinx.android.synthetic.main.fragment_subscription.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class SubscriptionFragment : Fragment(R.layout.fragment_subscription),View.OnClickListener {
+class SubscriptionFragment : Fragment(R.layout.fragment_subscription), View.OnClickListener {
 
     private lateinit var mRecyclerViewSubscription: RecyclerView
+    private lateinit var mButtonSubscriptionPayNow: Button
+
     private lateinit var mSubscriptionAdapter: SubscriptionAdapter
     private var mSubscriptionModelList: ArrayList<Subscription> = ArrayList()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.buttonPayNow.setOnClickListener(this)
+
         mRecyclerViewSubscription = view.findViewById(R.id.recyclerViewSubscription)
         mSubscriptionModelList.clear()
         mSubscriptionAdapter = context?.let {
@@ -34,19 +36,22 @@ class SubscriptionFragment : Fragment(R.layout.fragment_subscription),View.OnCli
         mRecyclerViewSubscription.adapter = mSubscriptionAdapter
         mSubscriptionAdapter.notifyDataSetChanged()
 
+        mButtonSubscriptionPayNow = view.findViewById(R.id.buttonSubscriptionPayNow)
+        mButtonSubscriptionPayNow.setOnClickListener(this)
+
         onLoadSubscriptions()
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.buttonPayNow -> view?.let {
+            R.id.buttonSubscriptionPayNow -> view?.let {
                 Navigation.findNavController(it)
                     .navigate(R.id.action_subscriptionFragment_to_paymentSuccessFragment)
             }
         }
     }
 
-    private fun onLoadSubscriptions(){
+    private fun onLoadSubscriptions() {
         mSubscriptionModelList.add(
             Subscription(
                 "1",
@@ -78,5 +83,4 @@ class SubscriptionFragment : Fragment(R.layout.fragment_subscription),View.OnCli
             )
         )
     }
-
 }
