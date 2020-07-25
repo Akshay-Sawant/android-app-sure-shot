d5e4afc535aa946c4a26e4e687804dd8b975ee91
Akshay Sawant <akshaysawant003@gmail.com>
2020-07-25 13:46:24 +0530

    LevelsDetailsFragment.kt: - created UI - binded all the views - updated strings.xml file

---
+++ app/src/main/java/com/sureshotdiscount/app/ui/referralslist/LevelsDetailsFragment.kt
@@ -0,0 +1,28 @@
+package com.sureshotdiscount.app.ui.referralslist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sureshotdiscount.app.R
+
+class LevelsDetailsFragment : Fragment(R.layout.fragment_levels_details) {
+
+    private lateinit var mTextViewLevelsDetailsUnsubscribed: TextView
+    private lateinit var mTextViewLevelsDetailsSubscribed: TextView
+
+    private lateinit var mTextViewLevelsDetailsNoDataFound: TextView
+    private lateinit var mRecyclerViewLevelsDetails: RecyclerView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mTextViewLevelsDetailsUnsubscribed =
+            view.findViewById(R.id.textViewLevelsDetailsUnsubscribed)
+        mTextViewLevelsDetailsSubscribed = view.findViewById(R.id.textViewLevelsDetailsSubscribed)
+
+        mTextViewLevelsDetailsNoDataFound = view.findViewById(R.id.textViewLevelsDetailsNoDataFound)
+        mRecyclerViewLevelsDetails = view.findViewById(R.id.recyclerViewLevelsDetails)
+    }
+}
