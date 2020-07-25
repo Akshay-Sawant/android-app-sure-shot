daef2f6c07c3b1a6be0f7c0a7a6176172b41a214
Akshay Sawant <akshaysawant003@gmail.com>
2020-07-25 14:44:13 +0530

    Configured share now feature but disabled it

--- app/src/main/java/com/sureshotdiscount/app/ui/referandearn/ReferEarnFragment.kt
+++ app/src/main/java/com/sureshotdiscount/app/ui/referandearn/ReferEarnFragment.kt
@@ -3,6 +3,7 @@ package com.sureshotdiscount.app.ui.referandearn
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context.CLIPBOARD_SERVICE
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -73,5 +74,16 @@ class ReferEarnFragment : Fragment(R.layout.fragment_refer_earn), View.OnClickLi
             ClipData.newPlainText(getString(R.string.text_your_referral_code), "Text to copy")
         clipboard?.setPrimaryClip(clip)
         Toast.makeText(context, "Copied", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onClickShareNow() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Sure Shot Discounts")
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, "Share Via")
+        startActivity(shareIntent)
     }
 }
