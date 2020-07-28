06525f5a9463e3863d5cb334904cd7337b8df760
Akshay Sawant <akshaysawant003@gmail.com>
2020-07-28 19:40:16 +0530

    PostpaidFragment.kt: - created progress bar and configured it - handled the visibility of button on selection of the spinner item - integrated the api and loaded the data into the spinner

--- app/src/main/java/com/sureshotdiscount/app/ui/postpaid/PostpaidFragment.kt
+++ app/src/main/java/com/sureshotdiscount/app/ui/postpaid/PostpaidFragment.kt
@@ -1,18 +1,32 @@
 package com.sureshotdiscount.app.ui.postpaid
 
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatSpinner
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.sureshotdiscount.app.R
+import com.sureshotdiscount.app.data.api.APIClient
+import com.sureshotdiscount.app.ui.prepaid.CircleListModel
+import com.sureshotdiscount.app.ui.prepaid.CircleModel
+import com.sureshotdiscount.app.utils.error.ErrorUtils
+import com.sureshotdiscount.app.utils.others.AlertDialogUtils
 import com.sureshotdiscount.app.utils.others.SharedPreferenceUtils
 import com.sureshotdiscount.app.utils.others.ValidationUtils
+import com.sureshotdiscount.app.utils.server.ServerInvalidResponseException
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PostpaidFragment : Fragment(R.layout.fragment_postpaid), View.OnClickListener {
 
@@ -23,7 +37,16 @@ class PostpaidFragment : Fragment(R.layout.fragment_postpaid), View.OnClickListe
     private lateinit var mTextInputLayoutPostPaidMobileNumber: TextInputLayout
     private lateinit var mTextInputEditTextPostPaidMobileNumber: TextInputEditText
 
+    private lateinit var mAppCompatSpinnerPostpaidLocation: AppCompatSpinner
+    private lateinit var mLocationAdapter: ArrayAdapter<String>
+    private var mLocationArrayList = ArrayList<String>()
+    private var mLocationCodeArrayList = ArrayList<String>()
+    private var mCircleModelListModelList: ArrayList<CircleListModel> = ArrayList()
+
     private lateinit var mButtonPostPaidProceed: Button
+
+    private lateinit var mContentLoadingProgressBarPostpaid: ContentLoadingProgressBar
+
     private lateinit var mSharedPreferenceUtils: SharedPreferenceUtils
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -40,13 +63,22 @@ class PostpaidFragment : Fragment(R.layout.fragment_postpaid), View.OnClickListe
         mTextInputEditTextPostPaidMobileNumber =
             view.findViewById(R.id.textInputEditTextPostPaidMobileNumber)
 
+        mAppCompatSpinnerPostpaidLocation = view.findViewById(R.id.appCompatSpinnerPostpaidLocation)
+        mAppCompatSpinnerPostpaidLocation.tag = getString(R.string.text_label_location)
+
         mButtonPostPaidProceed = view.findViewById(R.id.buttonPostPaidProceed)
         mButtonPostPaidProceed.setOnClickListener(this@PostpaidFragment)
+
+        mContentLoadingProgressBarPostpaid =
+            view.findViewById(R.id.contentLoadingProgressBarPostpaid)
     }
 
     override fun onResume() {
         super.onResume()
+        mContentLoadingProgressBarPostpaid.show()
         onLoadCompanyDetails()
+        onSelectItemFromSpinner()
+        onLoadLocation()
     }
 
     override fun onDetach() {
@@ -61,6 +93,31 @@ class PostpaidFragment : Fragment(R.layout.fragment_postpaid), View.OnClickListe
             }
             R.id.buttonPostPaidProceed -> isPostpaidValidated()
         }
+    }
+
+    private fun onSelectItemFromSpinner() {
+        mAppCompatSpinnerPostpaidLocation.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    when (mAppCompatSpinnerPostpaidLocation.selectedItemPosition) {
+                        0 -> {
+                            mButtonPostPaidProceed.visibility = View.GONE
+                        }
+                        else -> {
+                            mButtonPostPaidProceed.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            }
     }
 
     private fun onLoadCompanyDetails() {
@@ -91,6 +148,92 @@ class PostpaidFragment : Fragment(R.layout.fragment_postpaid), View.OnClickListe
                 view?.let {
                     Navigation.findNavController(it)
                         .navigate(R.id.action_recharge_to_rechargeDetails)
+                }
+            }
+        }
+    }
+
+    private fun onLoadLocation() {
+        context?.let { it ->
+            when {
+                APIClient.isNetworkConnected(it) -> {
+                    APIClient.apiInterface
+                        .getCircleList(mSharedPreferenceUtils.getLoggedInUser().loginToken)
+                        .enqueue(object : Callback<CircleModel> {
+                            override fun onResponse(
+                                call: Call<CircleModel>,
+                                response: Response<CircleModel>
+                            ) {
+                                if (response.isSuccessful) {
+                                    val mCircleModel: CircleModel? = response.body()
+                                    mContentLoadingProgressBarPostpaid.hide()
+
+                                    if (mCircleModel != null) {
+                                        if (mCircleModel.mStatus) {
+                                            mCircleModelListModelList =
+                                                mCircleModel.mResponse as ArrayList<CircleListModel>
+                                            mLocationArrayList.add(
+                                                0,
+                                                getString(R.string.text_label_location)
+                                            )
+                                            mLocationCodeArrayList.add(
+                                                0,
+                                                getString(R.string.text_label_location)
+                                            )
+                                            for (mItem in mCircleModel.mResponse.indices) {
+                                                mLocationArrayList.add(mCircleModelListModelList[mItem].mCircleName)
+                                                mLocationCodeArrayList.add(mCircleModelListModelList[mItem].mCircleCode)
+                                            }
+                                            mLocationAdapter = ArrayAdapter(
+                                                it,
+                                                android.R.layout.simple_spinner_dropdown_item,
+                                                mLocationArrayList
+                                            )
+                                            mLocationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                                            mAppCompatSpinnerPostpaidLocation.adapter =
+                                                mLocationAdapter
+                                            mLocationAdapter.notifyDataSetChanged()
+                                        } else {
+                                            AlertDialogUtils.getInstance().showAlert(
+                                                it,
+                                                R.drawable.ic_warning_black,
+                                                "Invalid credentials",
+                                                "Your token has been expired. Please do re-login to proceed",
+                                                getString(android.R.string.ok),
+                                                null,
+                                                DialogInterface.OnDismissListener {
+                                                    it.dismiss()
+                                                }
+                                            )
+                                        }
+                                    } else {
+                                        ErrorUtils.logNetworkError(
+                                            ServerInvalidResponseException.ERROR_200_BLANK_RESPONSE +
+                                                    "\nResponse: " + response.toString(),
+                                            null
+                                        )
+                                        AlertDialogUtils.getInstance()
+                                            .displayInvalidResponseAlert(it)
+                                    }
+                                }
+                            }
+
+                            override fun onFailure(
+                                call: Call<CircleModel>,
+                                t: Throwable
+                            ) {
+                                ErrorUtils.parseOnFailureException(
+                                    it,
+                                    call,
+                                    t
+                                )
+                                mContentLoadingProgressBarPostpaid.hide()
+                            }
+                        })
+                }
+                else -> {
+                    mContentLoadingProgressBarPostpaid.hide()
+                    AlertDialogUtils.getInstance().displayNoConnectionAlert(it)
                 }
             }
         }
