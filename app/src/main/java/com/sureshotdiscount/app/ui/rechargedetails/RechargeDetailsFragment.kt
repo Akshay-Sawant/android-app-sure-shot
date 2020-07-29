4e86a3c600153c22a5c17276ffa24f6987a489fc
Akshay Sawant <akshaysawant003@gmail.com>
2020-07-29 17:37:33 +0530

    RechargeDetailsFragment.kt: - created fields for subscription id - binded the ui - configured all the views and handled it by toggling it by means of recharge type

--- app/src/main/java/com/sureshotdiscount/app/ui/rechargedetails/RechargeDetailsFragment.kt
+++ app/src/main/java/com/sureshotdiscount/app/ui/rechargedetails/RechargeDetailsFragment.kt
@@ -1,7 +1,6 @@
 package com.sureshotdiscount.app.ui.rechargedetails
 
 import android.os.Bundle
-import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
@@ -14,7 +13,7 @@ import com.google.android.material.textfield.TextInputLayout
 import com.sureshotdiscount.app.R
 import com.sureshotdiscount.app.utils.others.SharedPreferenceUtils
 import com.sureshotdiscount.app.utils.others.ValidationUtils
-import java.nio.Buffer
+import kotlin.properties.Delegates
 
 class RechargeDetailsFragment : Fragment(R.layout.fragment_recharge_details), View.OnClickListener {
 
@@ -26,6 +25,11 @@ class RechargeDetailsFragment : Fragment(R.layout.fragment_recharge_details), Vi
     private lateinit var mTextViewRechargeDetailsChangeMethod: TextView
     private lateinit var mTextViewRechargeDetailsMobileNumber: TextView
 
+    private lateinit var mTextViewRechargeDetailsSubscriptionIdLabel: TextView
+    private lateinit var mTextViewRechargeDetailsSubscriptionId: TextView
+    private lateinit var mTextViewRechargeSubscriptionIdChange: TextView
+    private lateinit var mViewRechargeDetailsDividerSubscriptionId: View
+
     private lateinit var mTextInputLayoutRechargeDetailsEnterAmount: TextInputLayout
     private lateinit var mTextInputEditTextRechargeDetailsEnterAmount: TextInputEditText
     private lateinit var mTextViewRechargeDetailsSeePlan: TextView
@@ -33,6 +37,7 @@ class RechargeDetailsFragment : Fragment(R.layout.fragment_recharge_details), Vi
     private lateinit var mButtonRechargeDetailsProceed: Button
 
     private lateinit var mSharedPreferenceUtils: SharedPreferenceUtils
+    private var mIsMobileRecharge by Delegates.notNull<Boolean>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -52,11 +57,20 @@ class RechargeDetailsFragment : Fragment(R.layout.fragment_recharge_details), Vi
         mTextViewRechargeDetailsMobileNumber =
             view.findViewById(R.id.textViewRechargeDetailsMobileNumber)
 
+        mTextViewRechargeDetailsSubscriptionIdLabel =
+            view.findViewById(R.id.textViewRechargeDetailsSubscriptionIdLabel)
+        mTextViewRechargeDetailsSubscriptionId =
+            view.findViewById(R.id.textViewRechargeDetailsSubscriptionId)
+        mTextViewRechargeSubscriptionIdChange =
+            view.findViewById(R.id.textViewRechargeDetailsSubscriptionIdChange)
+        mTextViewRechargeSubscriptionIdChange.setOnClickListener(this@RechargeDetailsFragment)
+        mViewRechargeDetailsDividerSubscriptionId =
+            view.findViewById(R.id.viewRechargeDetailsDividerSubscriptionId)
+
         mTextInputLayoutRechargeDetailsEnterAmount =
             view.findViewById(R.id.textInputLayoutRechargeDetailsEnterAmount)
         mTextInputEditTextRechargeDetailsEnterAmount =
             view.findViewById(R.id.textInputEditTextRechargeDetailsEnterAmount)
-        mTextInputEditTextRechargeDetailsEnterAmount.isEnabled = false
 
         mTextViewRechargeDetailsSeePlan =
             view.findViewById(R.id.textViewRechargeDetailsSeePlan)
@@ -81,7 +95,8 @@ class RechargeDetailsFragment : Fragment(R.layout.fragment_recharge_details), Vi
             R.id.textViewRechargeDetailsChange -> view?.let {
                 Navigation.findNavController(it).popBackStack(R.id.myAccountFragment, false)
             }
-            R.id.textViewRechargeDetailsChangeMethod -> view?.let {
+            R.id.textViewRechargeDetailsChangeMethod,
+            R.id.textViewRechargeDetailsSubscriptionIdChange -> view?.let {
                 Navigation.findNavController(it).popBackStack()
             }
             R.id.textViewRechargeDetailsSeePlan -> view?.let {
@@ -99,6 +114,7 @@ class RechargeDetailsFragment : Fragment(R.layout.fragment_recharge_details), Vi
     private fun onLoadCompanyDetails() {
         context?.let {
             mSharedPreferenceUtils = SharedPreferenceUtils(it)
+            mIsMobileRecharge = mSharedPreferenceUtils.getIsMobileRecharge(it)
 
             Glide.with(it)
                 .load(mSharedPreferenceUtils.getRechargeCompanyLogo(it))
@@ -106,9 +122,29 @@ class RechargeDetailsFragment : Fragment(R.layout.fragment_recharge_details), Vi
                 .into(mAppCompatImageViewRechargeDetailsCompanyLogo)
             mTextViewRechargeDetailsCompanyName.text =
                 mSharedPreferenceUtils.getRechargeCompanyName(it)
-            mTextViewRechargeDetailsRechargeType.text = mSharedPreferenceUtils.getRechargeType(it)
             mTextViewRechargeDetailsMobileNumber.text =
                 mSharedPreferenceUtils.getRechargeMobileNumber(it)
+
+            if (mIsMobileRecharge) {
+                mTextViewRechargeDetailsSubscriptionIdLabel.visibility = View.GONE
+                mTextViewRechargeDetailsSubscriptionId.visibility = View.GONE
+                mTextViewRechargeSubscriptionIdChange.visibility = View.GONE
+                mViewRechargeDetailsDividerSubscriptionId.visibility = View.GONE
+
+                mTextInputEditTextRechargeDetailsEnterAmount.isEnabled = false
+                mTextViewRechargeDetailsSeePlan.visibility = View.VISIBLE
+            } else {
+                mTextViewRechargeDetailsSubscriptionIdLabel.visibility = View.VISIBLE
+                mTextViewRechargeDetailsSubscriptionId.visibility = View.VISIBLE
+                mTextViewRechargeSubscriptionIdChange.visibility = View.VISIBLE
+                mViewRechargeDetailsDividerSubscriptionId.visibility = View.VISIBLE
+
+                mTextViewRechargeDetailsSubscriptionId.text =
+                    mSharedPreferenceUtils.getRechargeSubscriptionId(it)
+
+                mTextInputEditTextRechargeDetailsEnterAmount.isEnabled = true
+                mTextViewRechargeDetailsSeePlan.visibility = View.GONE
+            }
             mTextInputEditTextRechargeDetailsEnterAmount.setText(
                 getString(
                     R.string.text_label_rupees,
